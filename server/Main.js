024f8b68307c329cbafa6c80f69a2432d1e7a48b
Joseph Ostrow <jstro@umich.edu>
2021-07-27 13:28:42 -0400

    update res.sendFile

--- server/Main.js
+++ server/Main.js
@@ -54,8 +54,8 @@ app.use(
 
 new Router(app, db)
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '../public', 'index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
