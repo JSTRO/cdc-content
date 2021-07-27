91a487140bd866c9f20df29698f7441b9b3977ee
Joseph Ostrow <jstro@umich.edu>
2021-07-27 13:13:35 -0400

    add cors

--- server/Main.js
+++ server/Main.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const path = require('path')
 const mysql = require('mysql')
 const session = require('express-session')
 const MySQLStore = require('express-mysql-session')(session)
 const Router = require('./Router')
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../build')))
 app.use(express.json())
 
 // Database
